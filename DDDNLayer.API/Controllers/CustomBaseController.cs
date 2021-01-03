444e7b5f36c6ac6ff1d5930292c814f7e0a42ce2
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-01-03 03:27:11 +0300

    Refactoring

---
+++ DDDNLayer.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDDNLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        protected IActionResult Response(ValidationResult validationResult, int successfullStatusCode)
+        {
+            if (validationResult.IsValid)
+            {
+            }
+            else
+            {
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ DDDNLayer.Domain.Core/Models/NoDataDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDNLayer.Domain.Core.Models
+{
+    public class NoDataDto
+    {
+    }
+}
